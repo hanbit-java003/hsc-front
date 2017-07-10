755e502ea82661be38d644263302e7ef6c16f4e6
nahcoos <hscsc515@gmail.com>
2017-07-10 16:43:41 +0900

    사용자정보에 방문국가 아이콘 추가

--- src/js/model/userInfo.js
+++ src/js/model/userInfo.js
@@ -2,25 +2,64 @@ module.exports = [{
     img: 'img/shk.jpg',
     id: '중기마눌',
     text: '인생은 나처럼',
-    travel: '우르크, 미국, 중국'
+    subMenus: [{
+        name: 'img/spain.png'
+    }, {
+        name: 'img/tk.png'
+    }, {
+        name: 'img/eng.png'
+    }, {
+        name: 'img/usa.png'
+    }]
 }, {
     img: 'img/mcw.jpg',
     id: '문대갈',
     text: '얼굴이 크죠',
-    travel: '일본'
+    subMenus: [{
+        name: 'img/spain.png'
+    }, {
+        name: 'img/ger.png'
+    }, {
+        name: 'img/kor.png'
+    }, {
+        name: 'img/cz.png'
+    }, {
+        name: 'img/cn.png'
+    }],
+    more: true
 }, {
     img: 'img/iu.jpg',
     id: '이지은',
     text: '오빠가 좋은걸',
-    travel: '제주도'
+    subMenus: [{
+        name: 'img/usa.png'
+    }, {
+        name: 'img/jpn.png'
+    }, {
+        name: 'img/kor.png'
+    }, {
+        name: 'img/tk.png'
+    }]
 }, {
     img: 'img/jennie.jpg',
     id: '제니',
     text: '안녕하세요',
-    travel: '호주, 뉴질랜드'
+    subMenus: [{
+        name: 'img/kor.png'
+    }, {
+        name: 'img/eng.png'
+    }]
 }, {
     img: 'img/ireane.jpg',
     id: '아이린',
     text: '일상을 여행처럼 여행을 일상처럼',
-    travel: '캄보디아'
+    subMenus: [{
+        name: 'img/cz.png'
+    }, {
+        name: 'img/cn.png'
+    }, {
+        name: 'img/jpn.png'
+    }, {
+        name: 'img/kor.png'
+    }]
 }];
