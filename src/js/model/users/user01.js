08f67647fb98b5cf7af236a4d3afd84bf5833d0f
nahcoos <hscsc515@gmail.com>
2017-07-13 15:19:10 +0900

    유저정보 모듈화

---
+++ src/js/model/users/user01.js
@@ -0,0 +1,16 @@
+module.exports = {
+    id: '중기마눌',
+    text: '인생은 나처럼',
+    diary: '11',
+    country: '4',
+    img: 'shk',
+    flag: [{
+        name: 'spain'
+    }, {
+        name: 'tk'
+    }, {
+        name: 'eng'
+    }, {
+        name: 'usa'
+    }]
+};
--- src/js/with.js
+++ src/js/with.js
@@ -2,3 +2,31 @@ require('bootstrap');
 require('../less/with.less');
 
 var common = require('./common');
+
+var users = require('./model/users/user01');
+var diarys = require('./model/users/diary');
+
+function getUsers(users) {
+    $('.with-user-profile').empty();
+
+    var template = require('../template/user-profile.hbs');
+    var html = template(users);
+
+    $('.with-user-profile').append(html);
+}
+
+function getDiarys(diarys) {
+    $('.with-user-contents').empty();
+
+    var template = require('../template/user-contents.hbs');
+
+    for (var i=0; i<diarys.length; i++) {
+        var html = template(diarys[i]);
+
+        $('.with-user-contents').append(html);
+    }
+
+}
+
+getUsers(users);
+getDiarys(diarys);
