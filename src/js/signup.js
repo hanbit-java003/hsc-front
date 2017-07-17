58b0d8670f85e180c06b05b2c8d379d6941df960
nahcoos <hscsc515@gmail.com>
2017-07-17 15:00:03 +0900

    회원가입페이지와 로그인페이지 연결

--- src/js/signup.js
+++ src/js/signup.js
@@ -2,3 +2,7 @@ require('bootstrap');
 require('../less/signup.less');
 
 var common = require('./common');
+
+$('.signup-option > div:last-child').on('click', function () {
+   location.href = './login.html';
+});
