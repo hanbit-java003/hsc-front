194f5c0cdcec2266b36d0ddad865107de92e09cc
nahcoos <hscsc515@gmail.com>
2017-07-18 17:10:00 +0900

    검색 페이지 모듈화

---
+++ src/js/model/search-page.js
@@ -0,0 +1,81 @@
+module.exports = [{
+    img: 'img01.jpg',
+    user: 'ireane.jpg',
+    userId: 'kaeunlovestrip',
+    title: '혼자서도 괜찮은 바르셀로나',
+    content: '바르셀로나에서 운이 어쩌고 저쩌고 저쩌고 저쩌고 어쩌고',
+    route: [{
+        name: '인천국제공항'
+    }, {
+        name: '바르셀로나 공항'
+    }, {
+        name: '하마드 공항'
+    }, {
+        name: '바르셀로나 숙소'
+    }, {
+        name: '에어비엔비 숙소'
+    }],
+    hashtag: '#스페인 #바르셀로나 #로맨틱 #성공적',
+    date: '2017.05.18 - 2017.05.25',
+    like: 8
+}, {
+    img: 'img01.jpg',
+    user: 'ireane.jpg',
+    userId: 'kaeunlovestrip',
+    title: '혼자서도 괜찮은 바르셀로나',
+    content: '바르셀로나에서 운이 어쩌고 저쩌고 저쩌고 저쩌고 어쩌고',
+    route: [{
+        name: '인천국제공항'
+    }, {
+        name: '바르셀로나 공항'
+    }, {
+        name: '하마드 공항'
+    }, {
+        name: '바르셀로나 숙소'
+    }, {
+        name: '에어비엔비 숙소'
+    }],
+    hashtag: '#스페인 #바르셀로나 #로맨틱 #성공적',
+    date: '2017.05.18 - 2017.05.25',
+    like: 8
+}, {
+    img: 'img01.jpg',
+    user: 'ireane.jpg',
+    userId: 'kaeunlovestrip',
+    title: '혼자서도 괜찮은 바르셀로나',
+    content: '바르셀로나에서 운이 어쩌고 저쩌고 저쩌고 저쩌고 어쩌고',
+    route: [{
+        name: '인천국제공항'
+    }, {
+        name: '바르셀로나 공항'
+    }, {
+        name: '하마드 공항'
+    }, {
+        name: '바르셀로나 숙소'
+    }, {
+        name: '에어비엔비 숙소'
+    }],
+    hashtag: '#스페인 #바르셀로나 #로맨틱 #성공적',
+    date: '2017.05.18 - 2017.05.25',
+    like: 8
+}, {
+    img: 'img01.jpg',
+    user: 'ireane.jpg',
+    userId: 'kaeunlovestrip',
+    title: '혼자서도 괜찮은 바르셀로나',
+    content: '바르셀로나에서 운이 어쩌고 저쩌고 저쩌고 저쩌고 어쩌고',
+    route: [{
+        name: '인천국제공항'
+    }, {
+        name: '바르셀로나 공항'
+    }, {
+        name: '하마드 공항'
+    }, {
+        name: '바르셀로나 숙소'
+    }, {
+        name: '에어비엔비 숙소'
+    }],
+    hashtag: '#스페인 #바르셀로나 #로맨틱 #성공적',
+    date: '2017.05.18 - 2017.05.25',
+    like: 8
+}];
--- src/js/search.js
+++ src/js/search.js
@@ -2,3 +2,19 @@ require('bootstrap');
 require('../less/search.less');
 
 var common = require('./common');
+
+var contents = require('./model/search-page');
+
+function initSeachContents(contents) {
+    $('.search-contents-tap').empty();
+
+    var template = require('../template/search/search-contents.hbs');
+
+    for (var i=0; i< contents.length; i++) {
+        var html = template(contents[i]);
+
+        $('.search-contents-tap').append(html);
+    }
+}
+
+initSeachContents(contents);
