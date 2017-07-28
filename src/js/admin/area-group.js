653b467ed91f19fcd4706c36f953695c38951494
nahcoos <hscsc515@gmail.com>
2017-07-28 16:07:13 +0900

    update admin area group db connection

--- src/js/admin/area-group.js
+++ src/js/admin/area-group.js
@@ -2,3 +2,18 @@ require('bootstrap');
 require('../../less/admin/area-group.less')
 
 var common = require('./common');
+
+
+$.ajax({
+    url: 'http://localhost:84/api/main/france',
+    success: function(result) {
+        initAreas(result);
+    }
+});
+
+function initAreas(area) {
+    var template = require('../../template/admin/ha-area.hbs');
+    var html = template(area);
+
+    $('.ha-area-groups > tbody').html(html);
+}
