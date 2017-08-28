f1ce88e39aaeefd5a7442d2c6d6386d6f91e2139
nahcoos <hscsc515@gmail.com>
2017-08-28 15:48:56 +0900

    로그인 페이지 ui 수정 및 체크박스 일괄선택 처리

--- src/js/signup.js
+++ src/js/signup.js
@@ -4,5 +4,24 @@ require('../less/signup.less');
 var common = require('./common');
 
 $('.signup-option > div:last-child').on('click', function () {
-   location.href = './login.html';
+    location.href = './login.html';
 });
+
+$('#under-age').on('click', function () {
+    alert('14세 미만은 서비스를 이용할 수 없습니다.');
+});
+
+
+$('#king').on('click', function () {
+    checkAll();
+});
+
+function checkAll() {
+    var king = document.getElementById('king');
+    var ch = document.getElementsByName('ch');
+
+    for (var i = 0; i < ch.length; i++) {
+        ch[i].checked = king.checked;
+    }
+
+}
