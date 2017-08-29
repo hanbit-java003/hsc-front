314dec8362964fa12aef523e91b845dc119ae812
nahcoos <hscsc515@gmail.com>
2017-08-29 15:25:55 +0900

    fixed bug

--- src/js/signup.js
+++ src/js/signup.js
@@ -9,21 +9,19 @@ $('.signup-option > div:last-child').on('click', function () {
 
 $('#under-age').on('click', function () {
     alert('14세 미만은 서비스를 이용할 수 없습니다.');
+    $('#under-age').prop('checked', false);
+    return;
 });
 
 
 $('#king').on('click', function () {
-    checkAll();
-});
-
-function checkAll() {
-    var king = document.getElementById('king');
-    var ch = document.getElementsByName('ch');
-
-    for (var i = 0; i < ch.length; i++) {
-        ch[i].checked = king.checked;
+    if ($('#king').prop('checked')) {
+        $("input[name=ch]").prop('checked', true);
     }
-}
+    else {
+        $("input[name=ch]").prop('checked', false);
+    }
+});
 
 function signUp() {
     var id = $('#signup-id').val().trim();
