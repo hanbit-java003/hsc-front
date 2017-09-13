e2bff4f2ea96b62bdd687e9c19de0826411f63e3
nahcoos <hscsc515@gmail.com>
2017-09-13 16:57:39 +0900

    update setting page image selector

--- src/js/setting.js
+++ src/js/setting.js
@@ -2,3 +2,24 @@ require('bootstrap');
 require('../less/setting.less');
 
 var common = require('./common');
+
+$('#setting-img-select').on('click', function () {
+    $('#setting-img-input').click();
+});
+
+$('#setting-img-input').on('change', function () {
+    if (this.files.length === 0) {
+        return;
+    }
+
+    var file = this.files[0];
+    var fileReader = new FileReader();
+
+    fileReader.addEventListener('load', function (event) {
+        var preview = event.target.result;
+
+        $('.setting-img').css('background-image', 'url(' + preview + ')');
+    });
+
+    fileReader.readAsDataURL(file);
+});
